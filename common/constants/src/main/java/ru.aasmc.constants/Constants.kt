12d5adb78513563c54bf9ba6cc4d2ee1517443de
Alex Alexeyev <aasmc@yandex.ru>
2022-02-04 14:37:19 +0300

    Move ApiKey to BuildConfig

--- common/constants/src/main/java/ru.aasmc.constants/Constants.kt
+++ common/constants/src/main/java/ru.aasmc.constants/Constants.kt
@@ -5,7 +5,6 @@ object ApiParameters {
     const val RAPID_API_HOST_VALUE = "wordsapiv1.p.rapidapi.com"
     const val RAPID_API_KEY_HEADER = "X-RapidAPI-Key"
     // TODO remove real key after publishing
-    const val RAPID_API_KEY_VALUE = "fd1ce1c1ebmsh1061b14fffe01f1p13e38fjsnbcf91cf2f5ee"
 }
 
 object ApiConstants {
