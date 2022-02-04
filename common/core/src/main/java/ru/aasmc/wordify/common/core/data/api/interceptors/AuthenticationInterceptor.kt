cea63b8a18f22398c20a8af873585b6ec31bb30a
Alex Alexeyev <aasmc@yandex.ru>
2022-02-04 14:40:08 +0300

    Move ApiKey to BuildConfig

--- common/core/src/main/java/ru/aasmc/wordify/common/core/data/api/interceptors/AuthenticationInterceptor.kt
+++ common/core/src/main/java/ru/aasmc/wordify/common/core/data/api/interceptors/AuthenticationInterceptor.kt
@@ -3,6 +3,7 @@ package ru.aasmc.wordify.common.core.data.api.interceptors
 import okhttp3.Interceptor
 import okhttp3.Response
 import ru.aasmc.constants.ApiParameters
+import ru.aasmc.wordify.common.core.BuildConfig
 import javax.inject.Inject
 
 class AuthenticationInterceptor @Inject constructor() : Interceptor {
@@ -11,7 +12,7 @@ class AuthenticationInterceptor @Inject constructor() : Interceptor {
         val hostHeader = ApiParameters.RAPID_API_HOST_HEADER
         val hostHeaderValue = ApiParameters.RAPID_API_HOST_VALUE
         val apiKeyHeader = ApiParameters.RAPID_API_KEY_HEADER
-        val apiKey = ApiParameters.RAPID_API_KEY_VALUE
+        val apiKey = BuildConfig.ApiKey
 
         val request = chain.request()
             .newBuilder()
